811abadb4ad623b85807d2d2ace8c88b72836998
EricMai2112 <mait58674@gmail.com>
2026-02-05 13:15:10 +0700

    chore/#: initiate project, add dependencies and related libraries, configuration and validation

---
+++ apps/bff/src/main.ts
@@ -0,0 +1,22 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
+
+bootstrap();
