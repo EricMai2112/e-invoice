fd47364d65a4f24cb76c632043ebae640406fd72
EricMai2112 <mait58674@gmail.com>
2026-02-08 00:50:32 +0700

    feat/#: TCP Configuration

--- apps/bff/src/app/app.controller.ts
+++ apps/bff/src/app/app.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ReponseDTO } from '@common/interfaces/gateway/response.interface';
+import { ResponseDTO } from '@common/interfaces/gateway/response.interface';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('TCP_INVOICE_SERVICE') private readonly invoiceClient: ClientProxy,
+  ) {}
 
   @Get()
   getData() {
     const result = this.appService.getData();
-    return new ReponseDTO({ data: result });
+    return new ResponseDTO({ data: result });
+  }
+
+  @Get('invoice')
+  async getInvoice() {
+    const result = await firstValueFrom(this.invoiceClient.send<string, number>('get_invoice', 1));
+    return new ResponseDTO({ data: result });
   }
 }
