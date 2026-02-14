b7f64e2805956aecc93c321f40e25d9ca2c8d2ac
EricMai2112 <mait58674@gmail.com>
2026-02-14 22:29:59 +0700

    chore/#: entity registration, build product repository

--- apps/product/src/app/modules/product/product.module.ts
+++ apps/product/src/app/modules/product/product.module.ts
@@ -1,10 +1,14 @@
 import { TypeOrmProvider } from '@common/configuration/type-orm.config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '@common/entities/product.entity';
+import { ProductController } from './controllers/product.controller';
+import { ProductService } from './services/product.service';
 
 @Module({
-  imports: [TypeOrmProvider],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmProvider, TypeOrmModule.forFeature([Product])],
+  controllers: [ProductController],
+  providers: [ProductService],
   exports: [],
 })
 export class ProductModule {}
