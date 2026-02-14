f087120daad0d2f21924fa067e5e44a12c12f1ba
EricMai2112 <mait58674@gmail.com>
2026-02-14 22:02:50 +0700

    chore/#: build TypeORM provider and connect to postgres

--- apps/product/src/app/modules/product/product.module.ts
+++ apps/product/src/app/modules/product/product.module.ts
@@ -1,8 +1,9 @@
+import { TypeOrmProvider } from '@common/configuration/type-orm.config';
 import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [TypeOrmProvider],
   controllers: [],
-  imports: [],
   providers: [],
   exports: [],
 })
