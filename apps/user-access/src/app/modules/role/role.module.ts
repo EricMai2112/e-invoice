2c46be7c61e685fff1bbdf52367ece3aa0f4cf43
EricMai2112 <mait58674@gmail.com>
2026-02-25 23:36:02 +0700

    feat/#: service and repository for user role

--- apps/user-access/src/app/modules/role/role.module.ts
+++ apps/user-access/src/app/modules/role/role.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RoleDestination } from '@commonjs/schemas/role.schema';
+import { RoleController } from './controllers/role.controller';
+import { RoleService } from './services/role.service';
+import { RoleRepository } from './repositories/role.repository';
 
 @Module({
   imports: [MongooseModule.forFeature([RoleDestination])],
-  controllers: [],
-  providers: [],
+  controllers: [RoleController],
+  providers: [RoleService, RoleRepository],
 })
 export class RoleModule {}
