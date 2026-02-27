e964cc687bfa224a8621c949cfe762e63d93487f
EricMai2112 <mait58674@gmail.com>
2026-02-27 23:09:38 +0700

    chore/#: adjust configuration for role module

--- apps/user-access/src/app/app.module.ts
+++ apps/user-access/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CONFIGURATION, TConfiguration } from '../configuration';
 import { RoleModule } from './modules/role/role.module';
-import { MongoProvider } from '@common/configuration/mongo.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] }), MongoProvider, RoleModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] }), RoleModule],
   controllers: [],
   providers: [],
 })
--- apps/user-access/src/app/modules/role/role.module.ts
+++ apps/user-access/src/app/modules/role/role.module.ts
@@ -4,9 +4,10 @@ import { RoleDestination } from '@commonjs/schemas/role.schema';
 import { RoleController } from './controllers/role.controller';
 import { RoleService } from './services/role.service';
 import { RoleRepository } from './repositories/role.repository';
+import { MongoProvider } from '@common/configuration/mongo.config';
 
 @Module({
-  imports: [MongooseModule.forFeature([RoleDestination])],
+  imports: [MongoProvider, MongooseModule.forFeature([RoleDestination])],
   controllers: [RoleController],
   providers: [RoleService, RoleRepository],
 })
