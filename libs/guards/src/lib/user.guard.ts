6170e259ae26be333d2af42595899258797fa81d
EricMai2112 <mait58674@gmail.com>
2026-05-09 23:54:36 +0700

    feat/#: work with user guard to handle verify token

--- libs/guards/src/lib/user.guard.ts
+++ libs/guards/src/lib/user.guard.ts
@@ -1,18 +1,59 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Injectable, CanActivate, ExecutionContext, Logger, UnauthorizedException, Inject } from '@nestjs/common';
+import { firstValueFrom, map, Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
 import { MetadataKeys } from '@common/constants/common.constant';
+import { getAccessToken } from '@common/utils/request.util';
+import { TCP_SERVICES } from '@common/configuration/tcp.config';
+import { TcpClient } from '@common/interfaces/tcp/common/tcp-client.interface';
+import { TCP_REQUEST_MESSAGE } from '@common/constants/enum/tcp-request-message.enum';
+import { AuthorizerResponse } from '@common/interfaces/tcp/authorizer';
 
 @Injectable()
 export class UserGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  //Passing UserGuard.name to know errors from what
+  private readonly logger = new Logger(UserGuard.name);
+
+  constructor(
+    private readonly reflector: Reflector,
+    @Inject(TCP_SERVICES.AUTHORIZER_SERVICE) private readonly authorizerClient: TcpClient,
+  ) {}
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const authOptions = this.reflector.get<{ secured: boolean }>(MetadataKeys.SECURED, context.getHandler());
+    const req = context.switchToHttp().getRequest();
 
     if (!authOptions?.secured) {
       return true;
     }
 
-    return true;
+    return this.verifyToken(req);
+  }
+
+  //this method has called TCP, async await so using Promise<boolean>
+  private async verifyToken(req: any): Promise<boolean> {
+    try {
+      const token = getAccessToken(req);
+      const processId = req(MetadataKeys.PROCESS_ID);
+
+      const result = await this.verifyUserToken(token, processId);
+      if (!result?.valid) {
+        throw new UnauthorizedException('Token is invalid');
+      }
+
+      return true;
+    } catch (error) {
+      this.logger.error({ error });
+      throw new UnauthorizedException('Token is invalid');
+    }
+  }
+
+  private async verifyUserToken(token: string, processId: string) {
+    return firstValueFrom(
+      this.authorizerClient
+        .send<AuthorizerResponse, string>(TCP_REQUEST_MESSAGE.AUTHORIZER.VERIFY_USER_TOKEN, {
+          data: token,
+          processId,
+        })
+        .pipe(map((data) => data.data)),
+    );
   }
 }
--- libs/interfaces/src/lib/tcp/authorizer/authorizer-response.interface.ts
+++ libs/interfaces/src/lib/tcp/authorizer/authorizer-response.interface.ts
@@ -1,3 +1,27 @@
+import { User } from '@commonjs/schemas/user.schema';
 import { LoginResponseDto } from '../../gateway/authorizer';
+import { PERMISSION } from '@common/constants/enum/role.enum';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { JwtPayload } from 'jsonwebtoken';
 
 export type LoginTcpResponse = LoginResponseDto;
+
+export class AuthorizedMetadata {
+  userId: string | undefined;
+  user: User | undefined;
+  permissions: PERMISSION[] | undefined;
+  jwt: JwtPayload | undefined;
+
+  constructor(payload?: Partial<AuthorizedMetadata>) {
+    Object.assign(this, payload);
+  }
+}
+
+export class AuthorizerResponse {
+  valid = false;
+  metadata = new AuthorizedMetadata();
+
+  constructor(payload?: Partial<AuthorizerResponse>) {
+    Object.assign(this, payload);
+  }
+}
