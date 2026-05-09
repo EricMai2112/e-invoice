b63391b19a6101e99ea238dedc435d2003024480
EricMai2112 <mait58674@gmail.com>
2026-05-10 00:28:33 +0700

    feat/#: handle verify token and compare to keycloak to authorization and authentication

--- apps/authorize/src/app/modules/authorizer/controllers/authorizer.controller.ts
+++ apps/authorize/src/app/modules/authorizer/controllers/authorizer.controller.ts
@@ -4,8 +4,9 @@ import { AuthorizerService } from '../services/authorizer.service';
 import { MessagePattern } from '@nestjs/microservices';
 import { TCP_REQUEST_MESSAGE } from '@common/constants/enum/tcp-request-message.enum';
 import { RequestParams } from '@common/decorators/request-param.decorator';
-import { LoginTcpRequest, LoginTcpResponse } from '@common/interfaces/tcp/authorizer';
+import { AuthorizerResponse, LoginTcpRequest, LoginTcpResponse } from '@common/interfaces/tcp/authorizer';
 import { Response } from '@common/interfaces/tcp/common/response.interface';
+import { ProcessId } from '@common/decorators/processId.decorator';
 
 @Controller()
 @UseInterceptors(TcpLoggingInterceptor)
@@ -17,5 +18,12 @@ export class AuthorizerController {
     const result = await this.authorizerService.login(params);
 
     return Response.success<LoginTcpResponse>(result);
+  }
+
+  @MessagePattern(TCP_REQUEST_MESSAGE.AUTHORIZER.VERIFY_USER_TOKEN)
+  async verifyUserToken(@RequestParams() token: string) {
+    const result = await this.authorizerService.verifyUserToken(token);
+
+    return Response.success<AuthorizerResponse>(result);
   }
 }
